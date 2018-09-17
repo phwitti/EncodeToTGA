b54bb25ee24f80b9b2dc9af9779b90ecafe847f4
Philipp M. Wittershagen <philippwittershagen@web.de>
2018-09-17 23:18:48 +0200

    Added support for uncompressed tga-encoding
    + Uncompressed tga files also end with v2-footer
    + Small Improvements of size of returned byte-array

--- EncodeToTGAExtension.cs
+++ EncodeToTGAExtension.cs
@@ -30,13 +30,23 @@ using System.IO;
 
 public static class EncodeToTGAExtension
 {
-    public static byte[] EncodeToTGA(this Texture2D _texture2D)
+    // == "TRUEVISION-XFile.\0" (ASCII)
+    static readonly byte[] c_arV2Signature = { 0x54, 0x52, 0x55, 0x45, 0x56, 0x49, 0x53, 0x49, 0x4F, 0x4E, 0x2D, 0x58, 0x46, 0x49, 0x4C, 0x45, 0x2E, 0x00 };
+
+    public enum Compression
+    {
+        None, RLE
+    }
+
+    public static byte[] EncodeToTGA(this Texture2D _texture2D, Compression _compression = Compression.RLE)
     {
         const int iTgaHeaderSize = 18;
         const int iBytesPerPixelRGB24 = 3; // 1 byte per channel (rgb)
         const int iBytesPerPixelARGB32 = 4; // ~ (rgba)
 
-        int iBytesPerPixel = _texture2D.format == TextureFormat.ARGB32 ? iBytesPerPixelARGB32 : iBytesPerPixelRGB24;
+        int iBytesPerPixel = _texture2D.format == TextureFormat.ARGB32 || _texture2D.format == TextureFormat.RGBA32
+            ? iBytesPerPixelARGB32
+            : iBytesPerPixelRGB24;
 
         //
 
@@ -45,10 +55,10 @@ public static class EncodeToTGAExtension
             using (BinaryWriter binaryWriter = new BinaryWriter(memoryStream))
             {
                 // Write TGA Header
-
+                
                 binaryWriter.Write((byte)0);                    // IDLength (not in use)
                 binaryWriter.Write((byte)0);                    // ColorMapType (not in use)
-                binaryWriter.Write((byte)10);                   // DataTypeCode == 10 (Runlength encoded RGB images)
+                binaryWriter.Write((byte)(_compression == Compression.None ? 2 : 10)); // DataTypeCode (10 == Runlength encoded RGB images)
                 binaryWriter.Write((short)0);                   // ColorMapOrigin (not in use)
                 binaryWriter.Write((short)0);                   // ColorMapLength (not in use)
                 binaryWriter.Write((byte)0);                    // ColorMapDepth (not in use)
@@ -59,76 +69,98 @@ public static class EncodeToTGAExtension
                 binaryWriter.Write((byte)(iBytesPerPixel * 8)); // Bits Per Pixel
                 binaryWriter.Write((byte)0);                    // ImageDescriptor (not in use)
 
-                // Write RLE Encoded Pixels
-                
                 Color32[] arPixels = _texture2D.GetPixels32();
 
-                const int iMaxPacketLength = 128;
-                int iPacketStart = 0;
-                int iPacketEnd = 0;
-
-                while (iPacketStart < arPixels.Length)
+                if (_compression == Compression.None)
                 {
-                    Color32 c32PreviousPixel = arPixels[iPacketStart];
-
-                    // Get current Packet Type
-                    RLEPacketType packetType = EncodeToTGAExtension.PacketType(arPixels, iPacketStart);
-                    
-                    // Find Packet End
-                    int iReadEnd = Mathf.Min(iPacketStart + iMaxPacketLength, arPixels.Length);
-                    for (iPacketEnd = iPacketStart + 1; iPacketEnd < iReadEnd; ++iPacketEnd)
+                    // Write all Pixels one after the other
+                    for (int iPixel = 0; iPixel < arPixels.Length; iPixel++)
                     {
-                        bool bPreviousEqualsCurrent = EncodeToTGAExtension.Equals(arPixels[iPacketEnd-1], arPixels[iPacketEnd]);
+                        Color32 c32Pixel = arPixels[iPixel];
+                        binaryWriter.Write(c32Pixel.b);
+                        binaryWriter.Write(c32Pixel.g);
+                        binaryWriter.Write(c32Pixel.r);
 
-                        // Packet End if change in Packet Type or if max Packet-Size reached
-                        if (packetType == RLEPacketType.RAW && bPreviousEqualsCurrent ||
-                            packetType == RLEPacketType.RLE && !bPreviousEqualsCurrent)
+                        if (iBytesPerPixel == iBytesPerPixelARGB32)
+                            binaryWriter.Write(c32Pixel.a);
+                    }
+
+                    binaryWriter.Write(0);          // Offset of meta-information (not in use)
+                    binaryWriter.Write(0);          // Offset of Developer-Area (not in use)
+                    binaryWriter.Write(c_arV2Signature); // ImageDescriptor (not in use)
+                }
+                else
+                {
+                    // Write RLE Encoded Pixels
+
+                    const int iMaxPacketLength = 128;
+                    int iPacketStart = 0;
+                    int iPacketEnd = 0;
+
+                    while (iPacketStart < arPixels.Length)
+                    {
+                        Color32 c32PreviousPixel = arPixels[iPacketStart];
+
+                        // Get current Packet Type
+                        RLEPacketType packetType = EncodeToTGAExtension.PacketType(arPixels, iPacketStart);
+
+                        // Find Packet End
+                        int iReadEnd = Mathf.Min(iPacketStart + iMaxPacketLength, arPixels.Length);
+                        for (iPacketEnd = iPacketStart + 1; iPacketEnd < iReadEnd; ++iPacketEnd)
                         {
-                            break;
-                        }
-                    }
+                            bool bPreviousEqualsCurrent = EncodeToTGAExtension.Equals(arPixels[iPacketEnd - 1], arPixels[iPacketEnd]);
 
-                    // Write Packet
-
-                    int iPacketLength = iPacketEnd - iPacketStart;
-
-                    switch (packetType)
-                    {
-                        case RLEPacketType.RLE:
-
-                            // Add RLE-Bit to PacketLength
-                            binaryWriter.Write((byte)((iPacketLength - 1) | (1 << 7)));
-
-                            binaryWriter.Write(c32PreviousPixel.b);
-                            binaryWriter.Write(c32PreviousPixel.g);
-                            binaryWriter.Write(c32PreviousPixel.r);
-
-                            if (_texture2D.format == TextureFormat.ARGB32)
-                                binaryWriter.Write(c32PreviousPixel.a);
-
-                            break;
-                        case RLEPacketType.RAW:
-
-                            binaryWriter.Write((byte)(iPacketLength - 1));
-
-                            for (int iPacketPosition = iPacketStart; iPacketPosition < iPacketEnd; ++iPacketPosition)
+                            // Packet End if change in Packet Type or if max Packet-Size reached
+                            if (packetType == RLEPacketType.RAW && bPreviousEqualsCurrent ||
+                                packetType == RLEPacketType.RLE && !bPreviousEqualsCurrent)
                             {
-                                binaryWriter.Write(arPixels[iPacketPosition].b);
-                                binaryWriter.Write(arPixels[iPacketPosition].g);
-                                binaryWriter.Write(arPixels[iPacketPosition].r);
-
-                                if (_texture2D.format == TextureFormat.ARGB32)
-                                    binaryWriter.Write(arPixels[iPacketPosition].a);
+                                break;
                             }
+                        }
 
-                            break;
+                        // Write Packet
+
+                        int iPacketLength = iPacketEnd - iPacketStart;
+
+                        switch (packetType)
+                        {
+                            case RLEPacketType.RLE:
+
+                                // Add RLE-Bit to PacketLength
+                                binaryWriter.Write((byte)((iPacketLength - 1) | (1 << 7)));
+
+                                binaryWriter.Write(c32PreviousPixel.b);
+                                binaryWriter.Write(c32PreviousPixel.g);
+                                binaryWriter.Write(c32PreviousPixel.r);
+
+                                if (iBytesPerPixel == iBytesPerPixelARGB32)
+                                    binaryWriter.Write(c32PreviousPixel.a);
+
+                                break;
+                            case RLEPacketType.RAW:
+
+                                binaryWriter.Write((byte)(iPacketLength - 1));
+
+                                for (int iPacketPosition = iPacketStart; iPacketPosition < iPacketEnd; ++iPacketPosition)
+                                {
+                                    Color32 c32Pixel = arPixels[iPacketPosition];
+                                    binaryWriter.Write(c32Pixel.b);
+                                    binaryWriter.Write(c32Pixel.g);
+                                    binaryWriter.Write(c32Pixel.r);
+
+                                    if (iBytesPerPixel == iBytesPerPixelARGB32)
+                                        binaryWriter.Write(c32Pixel.a);
+                                }
+
+                                break;
+                        }
+
+                        iPacketStart = iPacketEnd;
                     }
-
-                    iPacketStart = iPacketEnd;
                 }
             }
 
-            return memoryStream.GetBuffer();
+            return memoryStream.ToArray();
         }
     }
 
